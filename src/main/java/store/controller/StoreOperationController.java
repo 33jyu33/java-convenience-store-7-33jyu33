dee35b8e1775ca453a8df5cfb33614fc81c5cd9c
33jyu33 <jy04169@gmail.com>
2024-11-07 18:25:22 +0900

    feat(StoreOperationController): change file reading from BufferedReader to Scanner
    Function getProductScanner:
    - Instead of removing readProduct function
    - Divide a string by a line rather than by a space
    - return scanner

--- src/main/java/store/controller/StoreOperationController.java
+++ src/main/java/store/controller/StoreOperationController.java
@@ -1,24 +1,23 @@
 package store.controller;
 
-import java.io.FileReader;
+import store.model.Store;
+
+import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
+import java.util.Scanner;
 
 public class StoreOperationController {
     public void run() {
-
+        Store store = new Store(getProductScanner());
     }
 
+    private Scanner getProductScanner() {
+        final String productTextPath = "C:\\WoowaCourse\\java-convenience-store-7-33jyu33\\src\\main\\resources\\products.md";
 
-    private List<String> readProduct() {
-        final String READ_FILE_ERROR = "Product.md 파일을 읽는 데 에러 발생, 확인 요망.";
         try {
-            return Files.readAllLines(Paths.get("C:\\WoowaCourse\\java-convenience-store-7-33jyu33\\src\\main\\resources\\products.md"));
+            return new Scanner(new File(productTextPath));
         } catch (IOException e) {
-            System.out.println(READ_FILE_ERROR);
-            return null;
+            throw new IllegalArgumentException();
         }
     }
 }
