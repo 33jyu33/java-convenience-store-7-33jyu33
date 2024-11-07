63fc4effa98f51dd2d460b608ff6ca54a0b671e8
33jyu33 <jy04169@gmail.com>
2024-11-07 20:32:16 +0900

    feat(PromotionRepository): set promotions
    Function setPromotions:
    - Validate promotionContext
    - Put a name and new promotion object to promotions

---
+++ src/main/java/store/model/PromotionRepository.java
@@ -0,0 +1,34 @@
+package store.model;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
+
+public class PromotionRepository {
+    private final HashMap<String, Promotion> promotions = new HashMap<>();
+
+    public PromotionRepository(Scanner promotionScanner){
+        while(promotionScanner.hasNext()){
+            setPromotions(promotionScanner.nextLine());
+        }
+    }
+
+    private void setPromotions(String promotionContext){
+        List<String> promotionInfo = contextToList(promotionContext);
+        String promotionName = promotionInfo.removeFirst();
+        this.promotions.put(promotionName, new Promotion(promotionInfo));
+    }
+
+    private List<String> contextToList(String context) {
+        List<String> promotionInfo = Arrays.asList(context.strip().split(","));
+        validateInfoSize(promotionInfo);
+        return promotionInfo;
+    }
+
+    private void validateInfoSize(List<String> promotionInfo) {
+        if (promotionInfo.size() != 5) {
+            throw new IllegalArgumentException("promotion 등록 형식에 문제 있음");
+        }
+    }
+}
