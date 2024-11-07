6b62fd940fa0da885268a5b2f06bcf6103d5a390
33jyu33 <jy04169@gmail.com>
2024-11-07 19:45:19 +0900

    feat(Product): divide the string into a list and validate the size
    Function contextToList:
    - Divide the string by comma into a list and return it
    Function validateInfoSize:
    - Exception occurs if the list is not size 4

--- src/main/java/store/model/Product.java
+++ src/main/java/store/model/Product.java
@@ -1,15 +1,30 @@
 package store.model;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Product {
     private final String name;
     private final Integer price;
-    private Integer quantity;
     private final Promotion promotion;
 
-    public Product(String name, Integer price, Integer quantity, Promotion promotion){
-        this.name = name;
-        this.price = price;
-        this.quantity = quantity;
-        this.promotion = promotion;
+    private Integer quantity;
+
+    public Product(String productContext) {
+        List<String> productInfo = contextToList(productContext);
+        validateInfoSize(productInfo);
     }
+
+    private List<String> contextToList(String context) {
+        return Arrays.asList(context.strip().split(","));
+    }
+
+    private void validateInfoSize(List<String> productInfo) {
+        if (productInfo.size() != 4) {
+            throw new IllegalArgumentException("product 입력 형식에 문제 있음");
+        }
+    }
+
+
 }
