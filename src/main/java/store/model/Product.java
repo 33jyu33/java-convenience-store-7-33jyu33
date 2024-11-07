80e009029064c9afcfbfc2c8792200e2404cb83a
33jyu33 <jy04169@gmail.com>
2024-11-07 20:09:23 +0900

    feat(Product): set name and price
    Function getPrice:
    - Validate that priceContext consists of only numbers
    - Convert string type to integer type to return

--- src/main/java/store/model/Product.java
+++ src/main/java/store/model/Product.java
@@ -12,19 +12,36 @@ public class Product {
     private Integer quantity;
 
     public Product(String productContext) {
+        final int INDEX_NAME = 0;
+        final int INDEX_PRICE = 1;
+        final int INDEX_PROMOTION = 2;
+
         List<String> productInfo = contextToList(productContext);
-        validateInfoSize(productInfo);
+        this.name = productInfo.get(INDEX_NAME);
+        this.price = getPrice(productInfo.get(INDEX_PRICE));
     }
 
     private List<String> contextToList(String context) {
-        return Arrays.asList(context.strip().split(","));
+        List<String> productInfo = Arrays.asList(context.strip().split(","));
+        validateInfoSize(productInfo);
+        return productInfo;
     }
 
     private void validateInfoSize(List<String> productInfo) {
         if (productInfo.size() != 4) {
-            throw new IllegalArgumentException("product 입력 형식에 문제 있음");
+            throw new IllegalArgumentException("product 재고 등록 형식에 문제 있음");
         }
     }
 
+    private Integer getPrice(String priceContext){
+        validatePrice(priceContext);
+        return Integer.parseInt(priceContext);
+    }
+
+    private void validatePrice(String priceContext){
+        if (!priceContext.matches("^[0-9]+$")){
+            throw new IllegalArgumentException("product 재고 등록 가격은 숫자로만 구성해야 합니다");
+        }
+    }
 
 }
