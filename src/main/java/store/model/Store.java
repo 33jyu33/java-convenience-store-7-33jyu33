df8fef94e6b689b58aa405eebb0eb65cb959a696
33jyu33 <jy04169@gmail.com>
2024-11-08 12:27:02 +0900

    feat(Store): Combine information on all Product objects in stock into one string.
    Function getProductInformation:
    - Call getProductInformation from all products

--- src/main/java/store/model/Store.java
+++ src/main/java/store/model/Store.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class Store {
     private final List<Product> stock = new ArrayList<>();
 
-    public Store(Scanner productScanner, PromotionRepository promotionRepository){
+    public Store(Scanner productScanner, PromotionRepository promotionRepository) {
         // 첫 줄 제외
         productScanner.nextLine();
         while (productScanner.hasNextLine()) {
@@ -15,7 +15,16 @@ public class Store {
         }
     }
 
-    private void setStock(String product, PromotionRepository promotionRepository){
+    private void setStock(String product, PromotionRepository promotionRepository) {
         stock.add(new Product(product, promotionRepository));
+    }
+
+    public String getProductInformation() {
+        // 재고로 등록된 모든 상품의 정보를 한 문자열로 합친다.
+        StringBuilder productInformation = new StringBuilder();
+        for (Product product : stock) {
+            productInformation.append(product.getProductInformation());
+        }
+        return productInformation.toString();
     }
 }
