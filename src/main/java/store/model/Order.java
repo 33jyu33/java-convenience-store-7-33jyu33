11ff068f7c43e47af3ef0b67d89d5d0c32625542
33jyu33 <jy04169@gmail.com>
2024-11-09 17:21:08 +0900

    feat(Order): add a member variable promotionProducts as list type

--- src/main/java/store/model/Order.java
+++ src/main/java/store/model/Order.java
@@ -6,10 +6,16 @@ import java.util.List;
 
 public class Order {
     List<Product> soldProducts = new ArrayList<>();
+    List<Product> promotionProducts = new ArrayList<>();
     List<String> freeProduct;
     boolean membership = false;
+
     public Order(){
 
+    }
+
+    public void addPromotionProduct(Product product){
+        promotionProducts.add(product);
     }
 
     public void addProduct(Product product){
