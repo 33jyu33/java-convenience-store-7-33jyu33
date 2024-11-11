8253759db6d984a2a0f01cd10018df896ff7bbbc
33jyu33 <jy04169@gmail.com>
2024-11-11 17:17:32 +0900

    feat(Store): combine general and promotional products into one object
    Change the value of stock from Product list to Product
    Move the validation of the input file from Product

--- src/main/java/store/model/Store.java
+++ src/main/java/store/model/Store.java
@@ -3,7 +3,7 @@ package store.model;
 import java.util.*;
 
 public class Store {
-    private final Map<String, List<Product>> stock = new LinkedHashMap<>();
+    private final Map<String, Product> stock = new LinkedHashMap<>();
 
     public Store(Scanner productScanner, PromotionRepository promotionRepository) {
         // 첫 줄 제외
@@ -14,44 +14,41 @@ public class Store {
     }
 
     private void setStock(String productInput, PromotionRepository promotionRepository) {
-        List<Product> productType = new ArrayList<>();
-        Product product = new Product(productInput, promotionRepository);
-        if (stock.containsKey(product.getName())) {
-            productType.addAll(stock.get(product.getName()));
+        // 콜라,1000,3,반짝할인
+        List<String> productInfo = contextToList(productInput);
+
+        if (stock.containsKey(productInfo.getFirst())) {
+            stock.get(productInfo.getFirst()).update(productInfo.get(2), promotionRepository.getPromotion(productInfo.get(2)));
+            return;
         }
-        productType.add(product);
-        stock.put(product.getName(), productType);
+        Product product = new Product(productInfo, promotionRepository);
+        stock.put(product.getName(), product);
     }
 
     public String getProductInformation() {
         // 재고로 등록된 모든 상품의 정보를 한 문자열로 합친다.
         StringBuilder productInformation = new StringBuilder();
-        for (List<Product> products : stock.values()) {
-            for (Product product : products)
-                productInformation.append(product.getProductInformation());
+        for (Product product : stock.values()) {
+            productInformation.append(product.getProductInformation());
         }
         return productInformation.toString();
     }
 
     // 프로모션 부족 수량 확인, 재고 수량 확인
-    public List<Product> getSaleProduct(String name, Integer count) throws IllegalArgumentException {
-        Integer availableSale = 0;
-        Integer orderCount = count;
-        List<Product> saleProduct = new ArrayList<>();
-
-        for (Product product : stock.get(name)) {
-            saleProduct.add(product);
-            availableSale += product.getAvailableQuantity(orderCount);
-            if (availableSale.equals(count)) break;
-            orderCount -= availableSale;
-        }
-        isAvailableSale(availableSale, count);
+    public Product getSaleProduct(String name, Integer count) throws IllegalArgumentException {
+        Product saleProduct = stock.get(name);
+        saleProduct.validateOrderQuantity(count);
         return saleProduct;
     }
+    private List<String> contextToList(String context) {
+        List<String> productInfo = Arrays.asList(context.strip().split(","));
+        validateInfoSize(productInfo);
+        return productInfo;
+    }
 
-    private void isAvailableSale(Integer productCount, Integer orderCount){
-        if (!productCount.equals(orderCount)) {
-            throw new IllegalArgumentException("[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.");
+    private void validateInfoSize(List<String> productInfo) {
+        if (productInfo.size() != 4) {
+            throw new IllegalArgumentException("product 재고 등록 형식에 문제 있음");
         }
     }
 }
