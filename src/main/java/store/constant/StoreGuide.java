23027a76d0bfb62c19ef3d475a05c2bf1da62fea
33jyu33 <jy04169@gmail.com>
2024-11-11 22:42:40 +0900

    fix(Product): also expresses that it is out of stock

--- src/main/java/store/constant/StoreGuide.java
+++ src/main/java/store/constant/StoreGuide.java
@@ -2,7 +2,7 @@ package store.constant;
 
 public enum StoreGuide {
     START_STORE("안녕하세요. W편의점입니다.\n현재 보유하고 있는 상품입니다."),
-    PRODUCT("- %s %,d원 %,d개 %s\n"),
+    PRODUCT("- %s %,d원 %s %s\n"),
     GET_ORDER("구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])"),
     ASK_MEMBERSHIP("멤버십 할인을 받으시겠습니까? (Y/N)"),
     ASK_MORE("감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)"),
