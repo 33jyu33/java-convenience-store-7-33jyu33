0a9eeb279babac978feb8731260263c5d818250c
33jyu33 <jy04169@gmail.com>
2024-11-11 17:28:10 +0900

    feat(Product): add the number of promotions to the member variable
    Function update:
    - Update promotion information and quantity
    Function availableProductQuantity:
    - return the number of promotions applied
    Function getMembershipDiscount:
    - Return the membership discounted amount

--- src/main/java/store/model/Product.java
+++ src/main/java/store/model/Product.java
@@ -2,105 +2,131 @@ package store.model;
 
 import store.constant.StoreGuide;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 public class Product {
     private final String name;
     private final Integer price;
-    private final Promotion promotion;
 
+    private Promotion promotion;
     private Integer quantity;
+    private Integer promotionalQuantity;
 
-    public Product(String productContext, PromotionRepository promotionRepository) {
+    public Product(List<String> productInfo, PromotionRepository promotionRepository) {
         final int INDEX_NAME = 0;
         final int INDEX_PRICE = 1;
         final int INDEX_QUANTITY = 2;
         final int INDEX_PROMOTION = 3;
 
-        List<String> productInfo = contextToList(productContext);
         this.name = productInfo.get(INDEX_NAME);
         this.price = getPrice(productInfo.get(INDEX_PRICE));
         this.quantity = getPrice(productInfo.get(INDEX_QUANTITY));
         this.promotion = promotionRepository.getPromotion(productInfo.get(INDEX_PROMOTION));
+        this.promotionalQuantity = 0;
+        if (!Objects.equals(productInfo.get(INDEX_PROMOTION), "null")) {
+            this.promotionalQuantity = getPrice(productInfo.get(INDEX_QUANTITY));
+        }
     }
 
-    public Product(String name, Integer price, Integer quantity, Promotion promotion){
+    public void update(String quantity, Promotion promotion) {
+        this.quantity += getPrice(quantity);
+        if (promotion != null) {
+            this.promotion = promotion;
+            this.promotionalQuantity = getPrice(quantity);
+        }
+    }
+
+    public Product(String name, Integer price, Integer quantity, Promotion promotion, Integer promotionalQuantity) {
         this.name = name;
         this.price = price;
         this.quantity = quantity;
         this.promotion = promotion;
+        this.promotionalQuantity = promotionalQuantity;
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
 
-    public Integer getPrice(){
+    public Integer getPrice() {
         return price;
     }
 
-    public Integer getQuantity(){
+    public Integer getQuantity() {
         return quantity;
     }
 
-    public Integer getPromotionDiscount(){
-        return promotion.getFreeProductCount(quantity)*price;
-    }
-
-    public String getProductReceipt(){
-        return String.format("%s\t\t%,d\t%,d", name, quantity, price);
-    }
-
-    public String getPromotionReceipt(){
-        return String.format("%s\t\t%,d", name, quantity);
-    }
-
-    public String getProductInformation(){
-        String promotionName = "";
-        if(promotion != null){
-            promotionName = promotion.getName();
+    public void validateOrderQuantity(Integer quantity) {
+        if (this.quantity < quantity) {
+            throw new IllegalArgumentException("[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.");
         }
-        return String.format(StoreGuide.PRODUCT.getContext(), name, price, quantity,promotionName);
     }
 
-    public Integer getAvailableQuantity(Integer count){
-        if(count <= quantity) return count;
+    public Integer availableProductQuantity(Integer quantity) {
+        if (promotion == null) return null;
+        return promotion.getDiscountedProductCount(getPromotionalProductQuantity(quantity));
+    }
+
+    private Integer getPromotionalProductQuantity(Integer quantity) {
+        if (promotionalQuantity < quantity) return promotionalQuantity;
         return quantity;
     }
 
-    public void sold(Integer count){
+    public Integer getPromotionDiscount() {
+        if (promotion == null) return 0;
+        return promotion.getFreeProductCount(promotionalQuantity) * price;
+    }
+
+    public Integer getMembershipDiscount() {
+        if (promotion == null) return quantity * price;
+        return (quantity - promotion.getDiscountedProductCount(promotionalQuantity)) * price;
+    }
+
+    public String getProductReceipt() {
+        return String.format("%s\t\t%,d\t%,d", name, quantity, quantity * price);
+    }
+
+    public String getPromotionReceipt() {
+        if (promotion == null) return "";
+        Integer freeQuantity = promotion.getFreeProductCount(promotionalQuantity);
+        if (freeQuantity == 0) return "";
+        return String.format("%s\t\t%,d\n", name, freeQuantity);
+    }
+
+    public String getProductInformation() {
+        String information = "";
+        if (promotion != null) {
+            information += getPromotionalProductInformation();
+        }
+
+        return information + String.format(StoreGuide.PRODUCT.getContext(), name, price, quantity, "");
+    }
+
+    private String getPromotionalProductInformation() {
+        return String.format(StoreGuide.PRODUCT.getContext(), name, price, promotionalQuantity, promotion.getName());
+    }
+
+    public void sold(Integer count) {
+        if (promotion != null) {
+            promotionalQuantity -= availableProductQuantity(count);
+        }
         quantity -= count;
     }
 
-    public Product getSoldProduct(Integer count){
-        return new Product(name, price, count, promotion);
+    public Product getSoldProduct(Integer count) {
+        Integer promotionProductQuantity = availableProductQuantity(count);
+        if (promotion == null) promotionProductQuantity = 0;
+        return new Product(name, price, count, promotion, promotionProductQuantity);
     }
 
-    public boolean isPromotion(){
-        return promotion != null;
-    }
-
-    private List<String> contextToList(String context) {
-        List<String> productInfo = Arrays.asList(context.strip().split(","));
-        validateInfoSize(productInfo);
-        return productInfo;
-    }
-
-    private void validateInfoSize(List<String> productInfo) {
-        if (productInfo.size() != 4) {
-            throw new IllegalArgumentException("product 재고 등록 형식에 문제 있음");
-        }
-    }
-
-    private Integer getPrice(String priceContext){
+    private Integer getPrice(String priceContext) {
         validatePrice(priceContext);
         return Integer.parseInt(priceContext);
     }
 
-    private void validatePrice(String priceContext){
-        if (!priceContext.matches("^[0-9]+$")){
+    private void validatePrice(String priceContext) {
+        if (!priceContext.matches("^[0-9]+$")) {
             throw new IllegalArgumentException("product 재고 등록 가격은 숫자로만 구성해야 합니다");
         }
     }
