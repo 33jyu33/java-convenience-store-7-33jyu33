71c500354e23987fb44833479afdaef786b29967
33jyu33 <jy04169@gmail.com>
2024-11-11 17:18:36 +0900

    feat(StoreOperationController): combine general and promotional products into one object
    Function setOrder

--- src/main/java/store/controller/StoreOperationController.java
+++ src/main/java/store/controller/StoreOperationController.java
@@ -15,10 +15,14 @@ public class StoreOperationController {
     public void run() {
         final String productTextPath = "C:\\WoowaCourse\\java-convenience-store-7-33jyu33\\src\\main\\resources\\products.md";
         final String promotionTextPath = "C:\\WoowaCourse\\java-convenience-store-7-33jyu33\\src\\main\\resources\\promotions.md";
-
+        // 재고, 프로모션 등록
         PromotionRepository promotionRepository = new PromotionRepository(getScanner(promotionTextPath));
         Store store = new Store(getScanner(productTextPath), promotionRepository);
+
+        // 재고 출력
         printProductInformation(store);
+
+        // 주문 내역 저장
         Order order = setOrder(store);
         order.setMembership(InputView.askMembership());
         OutputView.receipt(order.getProducts(), order.getPromotion(), order.getResult());
@@ -41,33 +45,22 @@ public class StoreOperationController {
             Order order = new Order();
             try {
                 HashMap<String, Integer> orderMap = InputView.getOrder();
+                // [콜라-3],[사이다-10]
                 orderMap.forEach((name, count) -> {
-                    // 판매할 Product 객체 리스트
-                    List<Product> saleProducts = store.getSaleProduct(name, count);
-                    // 프로모션 물품의 개수
-                    Integer promotion = saleProducts.getFirst().getAvailableQuantity(count);
-                    if (!Objects.equals(promotion, count)) {
-                        System.out.printf("현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)", name, count - promotion);
-                        //Y이면 계속하기, N이면 끝내기
+                    // 판매할 Product 객체
+                    Product saleProduct = store.getSaleProduct(name, count);
+                    // 프로모션 적용 물품의 개수
+                    Integer promotionProductQuantity = saleProduct.availableProductQuantity(count);
+                    if (promotionProductQuantity!=null && !Objects.equals(promotionProductQuantity, count)) {
+                        if(!InputView.checkNotApplyPromotion(name, count - promotionProductQuantity)) return;
                     }
+
                     //Order에 판매된 프로모션 new Product 추가
-                    for (Product saleProduct : saleProducts) {
-                        if(saleProduct.isPromotion()) order.addPromotionProduct(saleProduct.getSoldProduct(promotion));
-                    }
-                    // Order에 판매된 new Product 추가(프로모션, 일반 합산)
-                    order.addProduct(saleProducts.getFirst().getSoldProduct(count));
-                });
+                    order.addProduct(saleProduct.getSoldProduct(count));
 
-                //판매한 물품은 제외하기
-                orderMap.forEach((name, count) -> {
-                    List<Product> soldProducts = store.getSaleProduct(name, count);
-                    Integer soldCount = soldProducts.getFirst().getAvailableQuantity(count);
-                    for (Product soldProduct : soldProducts) {
-                        soldProduct.sold(soldCount);
-                        soldCount = count - soldCount;
-                    }
+                    // 판매한 물품 제외하기
+                    saleProduct.sold(count);
                 });
-
                 return order;
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
