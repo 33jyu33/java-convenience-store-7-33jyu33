8b3b2030e6d317ec51bedc541db4b3ef21656189
33jyu33 <jy04169@gmail.com>
2024-11-11 22:04:28 +0900

    feat(InputView): ask additional free product

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -17,6 +17,13 @@ public class InputView {
         }
     }
 
+    public static boolean checkAdditionalFreeProduct(String name, Integer count){
+        System.out.printf("현재 %s은(는) %,d개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)\n", name, count);
+        String input = readLine();
+        validateYN(input);
+        return input.equals("Y");
+    }
+
     public static void checkNotApplyPromotion(String name, Integer quantity){
         System.out.printf("현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)\n", name, quantity);
         String input = readLine();
