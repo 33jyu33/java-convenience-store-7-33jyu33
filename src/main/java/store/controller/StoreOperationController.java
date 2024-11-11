a9353395a29dfb9940ef4f554400ba6567af20dc
33jyu33 <jy04169@gmail.com>
2024-11-11 22:02:16 +0900

    feat(StoreOperationController): check additional free product
    Function checkAdditionalCount:
    - Check the quantity of additional free product
    - Update quantity and promotionalQuantity

--- src/main/java/store/controller/StoreOperationController.java
+++ src/main/java/store/controller/StoreOperationController.java
@@ -60,20 +60,38 @@ public class StoreOperationController {
         Order order = new Order();
         orderMap.forEach((name, count) -> {
             Product saleProduct = store.getSaleProduct(name, count);
-            checkPromotionPeriod(saleProduct);
-            checkPromotionalProductCount(saleProduct, count, name);
-            order.addProduct(saleProduct.getSoldProduct(count));
+            Product soldProduct = getSoldProduct(saleProduct, name, count);
+            order.addProduct(soldProduct);
             saleProduct.sold(count);
         });
         return order;
     }
 
-    private void checkPromotionPeriod(Product saleProduct){
-        saleProduct.checkPromotionPeriod();
+    private Product getSoldProduct(Product saleProduct, String name, Integer count) {
+        Product soldProduct = saleProduct.getSoldProduct(count);
+        soldProduct.checkPromotionPeriod();
+        checkPromotionalProductCount(soldProduct, saleProduct, count, name);
+        return soldProduct;
     }
 
-    private void checkPromotionalProductCount(Product saleProduct, Integer count, String name) {
-        Integer promotionProductQuantity = saleProduct.availableProductQuantity(count);
+    private void checkPromotionalProductCount(Product soldProduct, Product saleProduct, Integer count, String name) throws IllegalArgumentException {
+        Integer promotionProductQuantity = soldProduct.availableProductQuantity(count);
+        if (checkAdditionalCount(promotionProductQuantity, soldProduct, saleProduct, count, name)) return;
+        checkNotApplyPromotion(promotionProductQuantity, name, count);
+    }
+
+    private boolean checkAdditionalCount(Integer promotionProductQuantity, Product soldProduct, Product saleProduct, Integer count, String name){
+        Integer additionalCount = soldProduct.getAdditionalFreeProductCount(count);
+        if ((promotionProductQuantity != null) && (0 < additionalCount) && (saleProduct.validateAdditionalQuantity(additionalCount+count))) {
+            if (InputView.checkAdditionalFreeProduct(name, additionalCount)) {
+                soldProduct.updatePromotionalQuantity(additionalCount+count);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void checkNotApplyPromotion(Integer promotionProductQuantity, String name, Integer count){
         if (promotionProductQuantity != null && !Objects.equals(promotionProductQuantity, count)) {
             InputView.checkNotApplyPromotion(name, count - promotionProductQuantity);
         }
